03721e0e623aaaca444316877b6bd8d7aa035d2b
saiaditya200 <adi20adi05@gmail.com>
2025-08-07 14:49:41 +0530

    change

--- Ecom-main/app.py
+++ Ecom-main/app.py
@@ -289,5 +289,5 @@ def logout():
         pass
     return redirect(url_for('login'))  # Redirect after logout
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5001)
 
